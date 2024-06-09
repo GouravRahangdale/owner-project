927b5a47ead92dc74f49dd3a220b95d098467a1a
GouravRahangdale <160835829+GouravRahangdale@users.noreply.github.com>
2024-06-09 10:23:40 +0530

    first commmit

---
+++ hello.java
@@ -0,0 +1,10 @@
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Gourav Rahangale");
+        System.out.println("Sangeeta Rana");
+        System.out.println("Pallavi");
+        System.out.println("Charu");
+    }
+
+}
